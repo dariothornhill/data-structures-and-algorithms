3bedec06dc4824af9ac02e9ba7e4c25cda7f6ee8
Dario Thornhill <dario.thornhill@gmail.com>
2021-02-22 19:50:57 +0100

    Added linked list imp

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,3 +1,7 @@
+class Node:
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
 class LinkedList:
     """
     Put docstring here
@@ -7,6 +11,27 @@ class LinkedList:
         # initialization here
         pass
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        node =  Node(value, self.head)
+        self.head = node
+
+    def append(self, value):
+        current = self.head
+        while current:
+            if current.next == None:
+                node = Node(value)
+                current.next = node
+                return
+            current = current.next
+
+
+    def insert_before(self, value):
+        node =  Node(value, self.head)
+        self.head = node
+
+    def insert_after(self, value, new_value):
+        current = self.head
+        while current:
+            if current.value == value:
+                current.next = Node(new_value, current.next)
+            current
